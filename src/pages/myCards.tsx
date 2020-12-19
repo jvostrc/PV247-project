711c4ee099d86ce79107d3b5d3fbb579c621a1a5
Lukáš <469427@mail.muni.cz>
2020-12-19 19:13:07 +0100

    merged

--- src/pages/myCards.tsx
+++ src/pages/myCards.tsx
@@ -3,21 +3,18 @@ import React, { FC } from "react";
 import TitleRow from "../components/TitleRow";
 
 const MyCards: FC = () => {
-
   document.title = "My Cards";
 
   return (
     <>
-      <TitleRow name={"My Cards"} showBack={true}></TitleRow>
-      <Grid container>
-        {/*data?.cards
+      {/* <TitleRow name={"My Cards"} showBack={true}></TitleRow> */}
+      <Grid container>{/*data?.cards
           .sort((a: IPkmnCard, b: IPkmnCard) => a?.number - b?.number)
           .map((item: IPkmnCard) => (
             <Grid key={item.id} lg={3} md={4} sm={6} xs={12}>
               <PkmnCard card={item} />
             </Grid>
-          ))*/}
-      </Grid>
+          ))*/}</Grid>
     </>
   );
 };
